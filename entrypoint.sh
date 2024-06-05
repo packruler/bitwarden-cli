469e9e5f2a0ffac2fe52488ddad6518e4e2ff050
Ethan Leisinger <packruler.git.noreply@anonmail.pro>
2024-06-05 12:34:18 -0600

    Use API key for logging in

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+if [[ -z "${BW_HOST}" || -z "${BW_CLIENTID}" || -z "${BW_CLIENTSECRET}" ]]; then
+  echo "Please set 'BW_HOST', 'BW_CLIENTID', and 'BW_CLIENTSECRET' environment variable"
+  exit 1
+fi
+
 bw config server ${BW_HOST}
 
-export BW_SESSION=$(bw login ${BW_USER} --passwordenv BW_PASSWORD --raw)
+export BW_SESSION=$(bw login --apikey)
 
 bw unlock --check
 
